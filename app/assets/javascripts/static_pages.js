cb201c23ae839d1b25c70b62c43a00a5a2f42e51
Grace Lo <gello57@gmail.com>
2014-01-18 09:51:58 -0500

    add piggy with short hand motion

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,9 @@
+/*$('#piggy').mouseover(function(){
+	$('.right-arm-wave',this).addClass();
+ // $(".left-arm").addClass("move");
+ // $(".right-arm").addClass("move2");
+}).mouseleave(function(){
+	$('.right-arm');
+ //   $(".left-arm").removeClass("move");
+//  $(".right-arm").removeClass("move2");
+});*/
